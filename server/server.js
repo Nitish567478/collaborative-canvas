30eeb979e56901c78aba99c900d3605d6a908115
Nitish <demo.nitishgrd147@gmail.com>
2026-01-30 21:51:25 +0530

    Use dynamic port for Render deployment

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ io.on("connection", socket => {
   socket.on("JOIN_ROOM", data => rooms.join(socket, data));
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
